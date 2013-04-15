daae82386284e6e7d4ac8c2e480b3f08906bdc63
Fliz <elizabeth.hamlet@hotmail.co.uk>
2013-04-15 16:29:38 +0100

    Fix to DateTime picker for Month

--- src/com/myleakyconlondon/ui/DatePickerFragment.java
+++ src/com/myleakyconlondon/ui/DatePickerFragment.java
@@ -5,6 +5,7 @@ import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.util.Log;
 import android.widget.*;
 
 import java.text.SimpleDateFormat;
@@ -39,7 +40,7 @@ public class DatePickerFragment extends DialogFragment implements DatePickerDial
 
         final Calendar c = Calendar.getInstance();
         int year = c.get(Calendar.YEAR);
-        int month = c.get(Calendar.MONTH);
+        int month = c.get(Calendar.MONTH + 1);
         int day = c.get(Calendar.DAY_OF_MONTH);
 
         return new DatePickerDialog(getActivity(), this, year, month, day);
